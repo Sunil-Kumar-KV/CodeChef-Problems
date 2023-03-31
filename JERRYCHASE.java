416d9df524761716f1da1a24f9d8abac23f3a0bf
Sunil Kumar K V <111721933+Sunil-Kumar-KV@users.noreply.github.com>
2023-03-31 09:03:05 +0530

    Add files via upload

---
+++ JERRYCHASE.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class TAG 
+{
+	public static void main (String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		int testcase = sc.nextInt();
+		while(testcase-- > 0)
+		{
+		    int x = sc.nextInt();
+		    int y = sc.nextInt();
+		    if(x<y)
+		    {
+		        System.out.println("yes");
+		    }
+		    else
+		    {
+		        System.out.println("no");
+		    }
+		}
+	}
+}
